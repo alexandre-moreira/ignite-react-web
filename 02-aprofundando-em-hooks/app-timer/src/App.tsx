6829ca1a57b3771154923b6483b5461246c32c3a
Alexandre Moreira <4lexandre.moreira@gmail.com>
2023-12-27 14:20:49 -0300

    styles: created themes

--- 02-aprofundando-em-hooks/app-timer/src/App.tsx
+++ 02-aprofundando-em-hooks/app-timer/src/App.tsx
@@ -1,8 +1,17 @@
+ import { ThemeProvider } from 'styled-components';
+import { Button } from "./components/Button";
+import { defaultTheme } from './styles/themes/deafult';
+
 export function App() {
   return (
-    <>
+    <ThemeProvider theme={defaultTheme}>
       <h2>App - Timer</h2>
-    </>
+      <Button variant="primary"/>
+      <Button variant="secondary"/>
+      <Button variant="danger"/>
+      <Button variant="success"/>
+      
+    </ThemeProvider>
   )
 }
 
