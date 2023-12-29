d88def5e2a36ecb2e89d24bfa288b69880107a38
Alexandre Moreira <4lexandre.moreira@gmail.com>
2023-12-28 23:31:35 -0300

    feat: created page home

--- 02-aprofundando-em-hooks/app-timer/src/App.tsx
+++ 02-aprofundando-em-hooks/app-timer/src/App.tsx
@@ -1,16 +1,12 @@
 import { ThemeProvider } from 'styled-components'
-import { Button } from './components/Button'
 import { defaultTheme } from './styles/themes/deafult'
 import { GlobalStyle } from './styles/global'
+import { Home } from './pages/Home'
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <h2>App - Timer</h2>
-      <Button variant="primary" />
-      <Button variant="secondary" />
-      <Button variant="danger" />
-      <Button variant="success" />
+      <Home />
       <GlobalStyle />
     </ThemeProvider>
   )
